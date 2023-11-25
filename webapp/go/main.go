3220d90998d05cb725e44cf81f1d388798781761
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-11-25 11:14:10 +0900

    middleware loggerを止めた

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -20,7 +20,6 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo-contrib/session"
@@ -142,7 +141,7 @@ func main() {
 	e := echo.New()
 	e.Debug = false
 	e.Logger.SetLevel(echolog.WARN)
-	e.Use(middleware.Logger())
+	// e.Use(middleware.Logger())
 	cookieStore := sessions.NewCookieStore(secret)
 	cookieStore.Options.Domain = "*.u.isucon.dev"
 	e.Use(session.Middleware(cookieStore))
