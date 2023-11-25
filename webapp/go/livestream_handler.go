cb51c6f16ad427387aa35a40e2b363b7d4809997
motoki317 <motoki317@gmail.com>
2023-11-25 11:55:25 +0900

    improve select tag N+1

--- webapp/go/livestream_handler.go
+++ webapp/go/livestream_handler.go
@@ -494,23 +494,30 @@ func fillLivestreamResponse(ctx context.Context, tx *sqlx.Tx, livestreamModel Li
 		return Livestream{}, err
 	}
 
-	var livestreamTagModels []*LivestreamTagModel
-	if err := tx.SelectContext(ctx, &livestreamTagModels, "SELECT * FROM livestream_tags WHERE livestream_id = ?", livestreamModel.ID); err != nil {
-		return Livestream{}, err
-	}
-
-	tags := make([]Tag, len(livestreamTagModels))
-	for i := range livestreamTagModels {
-		tagModel := TagModel{}
-		if err := tx.GetContext(ctx, &tagModel, "SELECT * FROM tags WHERE id = ?", livestreamTagModels[i].TagID); err != nil {
+	/*
+		var livestreamTagModels []*LivestreamTagModel
+		if err := tx.SelectContext(ctx, &livestreamTagModels, "SELECT * FROM livestream_tags WHERE livestream_id = ?", livestreamModel.ID); err != nil {
 			return Livestream{}, err
 		}
+	*/
 
-		tags[i] = Tag{
-			ID:   tagModel.ID,
-			Name: tagModel.Name,
-		}
+	var tags []Tag
+	if err := tx.SelectContext(ctx, &tags, "SELECT * FROM tags t JOIN livestream_tags lt ON lt.tag_id = t.id WHERE lt.livestream_id = ?", livestreamModel.ID); err != nil {
+		return Livestream{}, err
 	}
+	/*
+		for i := range livestreamTagModels {
+			tagModel := TagModel{}
+			if err := tx.GetContext(ctx, &tagModel, "SELECT * FROM tags WHERE id = ?", livestreamTagModels[i].TagID); err != nil {
+				return Livestream{}, err
+			}
+
+			tags[i] = Tag{
+				ID:   tagModel.ID,
+				Name: tagModel.Name,
+			}
+		}
+	*/
 
 	livestream := Livestream{
 		ID:           livestreamModel.ID,
