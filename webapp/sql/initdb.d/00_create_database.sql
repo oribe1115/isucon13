586006a5bd5d932f98ea9c28cef4f6cbc6676a15
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-11-25 12:48:32 +0900

    userのdropをやめる

--- webapp/sql/initdb.d/00_create_database.sql
+++ webapp/sql/initdb.d/00_create_database.sql
@@ -1,12 +1,12 @@
 DROP DATABASE `isupipe`;
 CREATE DATABASE IF NOT EXISTS `isupipe`;
 
-DROP USER IF EXISTS `isucon`@`%`;
+-- DROP USER IF EXISTS `isucon`@`%`;
 CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
 GRANT ALL PRIVILEGES ON isupipe.* TO 'isucon'@'%';
 
 CREATE DATABASE IF NOT EXISTS `isudns`;
 
-DROP USER IF EXISTS `isudns`@`%`;
+-- DROP USER IF EXISTS `isudns`@`%`;
 CREATE USER 'isudns'@'%' IDENTIFIED BY 'isudns';
 GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'%';
