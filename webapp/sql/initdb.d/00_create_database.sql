398ee4fb9d2a5abfebf142c8b2260ebd67269f97
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-11-25 12:56:25 +0900

    都度ユーザー作り直すのやめた

--- webapp/sql/initdb.d/00_create_database.sql
+++ webapp/sql/initdb.d/00_create_database.sql
@@ -2,11 +2,11 @@ DROP DATABASE `isupipe`;
 CREATE DATABASE IF NOT EXISTS `isupipe`;
 
 -- DROP USER IF EXISTS `isucon`@`%`;
-CREATE USER IF NOT EXISTS 'isucon'@'%' IDENTIFIED BY 'isucon';
-GRANT ALL PRIVILEGES ON isupipe.* TO 'isucon'@'%';
+-- CREATE USER IF NOT EXISTS 'isucon'@'%' IDENTIFIED BY 'isucon';
+-- GRANT ALL PRIVILEGES ON isupipe.* TO 'isucon'@'%';
 
 CREATE DATABASE IF NOT EXISTS `isudns`;
 
 -- DROP USER IF EXISTS `isudns`@`%`;
-CREATE USER IF NOT EXISTS 'isudns'@'%' IDENTIFIED BY 'isudns';
-GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'%';
+-- CREATE USER IF NOT EXISTS 'isudns'@'%' IDENTIFIED BY 'isudns';
+-- GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'%';
